e7057bb9c790f4d78cc648ab38e8dbfd7e750647
zhenhua <1641605908@qq.com>
2022-06-22 14:28:30 +0800

    initJavaBase

---
+++ src/main/java/com/czh/jdkbase/JdkbaseApplication.java
@@ -0,0 +1,13 @@
+package com.czh.jdkbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdkbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czh/jdkbase/JdkbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czh.jdkbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
